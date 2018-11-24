bf40ca826fbd86d480f72d79e9af30489b9b1c14
Smriti Tiwari <40840674+seepls@users.noreply.github.com>
2018-11-24 10:36:14 +0530

    Create info.js

---
+++ Brain.js/info.js
@@ -0,0 +1,10 @@
+//set of JS libraries for training neural networks and Naive-Bayesian classifier.
+
+//setup Brain.js : 
+install  : npm install brain.js
+
+//can include the lib in the browser including : 
+<script src="https://raw.githubusercontent.com/harthur-org/brain.js/master/browser.js"></script>
+
+//install naive bayesian classifier  using
+npm install classifier.
