0bacf2835a42d746d29fc89bb5a4a0fef88e60c1
Smriti Tiwari <40840674+seepls@users.noreply.github.com>
2018-11-24 10:29:46 +0530

    Create info.js

---
+++ KerasJS/info.js
@@ -0,0 +1,12 @@
+/*can run KerasModel in browser with GPU support using WebGL 
+list of keras model which can be run on browser 
+1.basic convent for MNIST 
+2.Convolutional variational autoencoder , trained on MNIST
+3.Auxilary classifier generative adversarial networks (AC-GAN)  on MNIST
+4.50-layer residual network, trained on ImageNet
+5.Inception v3, trained on ImageNet
+6.DenseNet-121, trained on ImageNet
+7.SqueezeNet v1.1, trained on ImageNet
+8.Bidirectional LSTM for IMDB sentiment classification
+
+*/
