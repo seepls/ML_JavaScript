0894753084947234d7c37a76ddb3aec78fe3207b
Smriti Tiwari <40840674+seepls@users.noreply.github.com>
2018-11-24 09:58:55 +0530

    Create info.js

---
+++ DeepLearn.js/info.js
@@ -0,0 +1,14 @@
+/*opensource ml JS lib by google 
+can be used for say 
+1 . training neural networks in browser 
+2.understanding ML models 
+
+
+You can run pre-trained models in inference mode.
+Ypi can get started very quickly by including the following code 
+within a head tag in the HTML file and writing JS programs for building the model.*/
+
+
+<script src="https://cdn.jsdelivr.net/npm/deeplearn@latest"></script>
+<!-- or -->
+<script src="https://unpkg.com/deeplearn@latest"></script>
