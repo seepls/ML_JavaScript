f38ea9ec7e5eea3c4c8d96fb16f3c2781d618fbc
Smriti Tiwari <40840674+seepls@users.noreply.github.com>
2018-11-24 10:04:17 +0530

    Create info.js

---
+++ propelJS/info.js
@@ -0,0 +1,8 @@
+/*propel is JS lib : provides GPU (Graphical processing unit ) numpy like structure for scientific computing
+used for NodeJS apps and in the browser .
+set up code for browser::: */
+<script src="https://unpkg.com/propel@3.1.0"></script>
+
+// for nodeJS app:
+npm install propel
+import { grad } from "propel";
