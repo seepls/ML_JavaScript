7e40e02129628e01fa3f9cf4eb645a508509893d
Smriti Tiwari <40840674+seepls@users.noreply.github.com>
2018-11-24 10:13:25 +0530

    Create info.js

---
+++ ML-JS/info.js
@@ -0,0 +1,26 @@
+//ML-JS provides machine learning tools for working with NodeJS and browsers. 
+
+//ML-JS tool can be set up using :
+<script src="https://www.lactame.com/lib/ml/2.2.0/ml.min.js"></script>
+
+following ML algorithms are supported :
+:: unsupervised 
+
+1.Principle component analysis (PCA)
+2.K-means clustering
+
+supervised::
+1.simple linear regression
+2.Multi-variate linear regression
+3.support vector machine
+4.naive bayes
+5.K-nearest neighbour 
+6.partial least square 
+7.Decision tree :CART
+8.Random forest
+9.logistic regression
+
+:::ANN
+1.feedforward neural network 
+
+
