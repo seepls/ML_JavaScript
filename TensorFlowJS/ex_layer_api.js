f01ebc873464f3374c7b5440f68b20e23f1e962f
Smriti Tiwari <40840674+seepls@users.noreply.github.com>
2018-11-25 13:10:24 +0530

    Update ex_layer_api.js

--- TensorFlowJS/ex_layer_api.js
+++ TensorFlowJS/ex_layer_api.js
@@ -7,10 +7,26 @@
  const model  =tf.sequential ({
  
  layers :[
+  //encoders
  tf.layers.lstm({units :hiddenSize ,inputShape :[maxLen , vocabSize]}),
  tf.layers.repeatVector({n:digits +1 }),
- 
+ //decoders 
+  tf.layers.lstm({units: hiddenSize , returnSequences: true }),
+  tf.layers.timeDistributed(tf.layers.dense({units: vocabSize}),
+  tf.layers.activation({activation: 'softmax'})
+                            
  ]});
 
-
+  // that is our model : now compile it with a loss 
   
+  model.compile ({
+  loss : ' categoricalCrossentropy',
+  optimizer : 'adam',
+  metrics: ['accuracy']
+ });
+
+const history  = await model.fit({x: data.xs , y: data.ys , epochs: 1 , batchSize});
+
+
+// model.fit : asynchronous call : they await keyword helps :  model.fit : can take 30-40 sec . call back with an history object and till tell GPU can work 
+
